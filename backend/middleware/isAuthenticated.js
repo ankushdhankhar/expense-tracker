56e3942bbc5df32c1953ded9fb36939f752a24ee
Ankush Dhankhar <ankushdhankhar3002@gmail.com>
2025-07-15 18:28:48 +0530

    availing cookie cross-site by changing token samesite

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -1,28 +1,30 @@
-import jwt from "jsonwebtoken" ;
+import jwt from "jsonwebtoken";
 
-const isAuthenticated = async (req ,res , next) =>{
-    try {
-        const token = req.cookies.token ;
-        if(!token){
-            return res.status(401).json({
-                message:"user not authenticated" ,
-                success:false
-            })
-        };
-        const decode = await jwt.verify(token , process.env.SECRET_KEY) ;
-        if(!decode){
-            return res.status(401).json({
-                message:"invalid token" ,
-                success:false
-            })
-        };
-        req.id = decode.userId ;
-        next() ;
-
-    } catch (error) {
-        console.log(error);
-        
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({
+        message: "user not authenticated",
+        success: false,
+      });
     }
-}
+    const decode = await jwt.verify(token, process.env.SECRET_KEY);
+    if (!decode) {
+      return res.status(401).json({
+        message: "invalid token",
+        success: false,
+      });
+    }
+    req.user = decode;
+    next();
+  } catch (error) {
+    console.log("Auth error:", error);
+    return res.status(401).json({
+      message: "Authentication failed",
+      success: false,
+    });
+  }
+};
 
-export default isAuthenticated ;
+export default isAuthenticated;
