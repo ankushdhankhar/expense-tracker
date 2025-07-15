aa9aecb61029daf40f48e82046f79e6e7174f02f
Ankush Dhankhar <ankushdhankhar3002@gmail.com>
2025-07-15 18:12:45 +0530

    frontend url update

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json()) ;
 app.use(urlencoded({extended:true}));
 app.use(cookieParser()) ;
 const corsOptions = {
-    origin : "http://localhost:5173" ,
+    origin : "https://expense-tracker-frontend-jnj7.onrender.com" ,
     credentials : true
 }
 app.use(cors(corsOptions));
