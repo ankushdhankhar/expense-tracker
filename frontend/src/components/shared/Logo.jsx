00e15ba464a61962194d6fd38472c61bbe6f947b
Ankush Dhankhar <ankushdhankhar3002@gmail.com>
2025-07-15 18:54:38 +0530

    logo

--- frontend/src/components/shared/Logo.jsx
+++ frontend/src/components/shared/Logo.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/expenselogo.png'
 
 const Logo = () => {
   return (
         <Link to = "/">
-            <img src={logo} alt="logo" className='w-14' />
+            <img src="/expenselogo.png" alt="logo" className='w-14' />
         </Link>
   )
 }
